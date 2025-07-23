80a6a6e567360d8bd124bc508f16778467ba9d7c
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2025-07-23 15:50:02 +0600

    feat: add deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://hasansujon786.github.io',
   integrations: [react()],
 
   vite: {
